e190a421b701486765275dc5cdd9cd374c5c1f48
marcodem11 <marcodemicheleta01@outlook.it>
2022-02-23 10:15:37 +0100

    ;

--- app.js
+++ app.js
@@ -5,3 +5,30 @@ function getRandomIntInclusive(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+const min=1;
+const max=100;
+const numeriDaIndovinare = [];
+const numeriUtente = [];
+
+do {
+
+  const number = getRandomIntInclusive(min, max);
+
+  if(numeriDaIndovinare.includes(number) === false ){
+      numeriDaIndovinare.push(number);
+  }
+} while (numeriDaIndovinare.length < 5)
+
+console.log(numeriDaIndovinare);
+
+alert(`Memorizza questi 5 numeri: ${numeriDaIndovinare.join(',')}.`);
+
+setTimeout( ()=> {
+
+    do {
+        
+    } while (numeriUtente.length < numeriDaIndovinare.length);
+    const num = parseInt(prompt('Inserisci un numero.'));
+}, 2000);
+    
