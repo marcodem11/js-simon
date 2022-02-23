289b724eef3d5d36b91991650574b88596c6cd5c
marcodem11 <marcodemicheleta01@outlook.it>
2022-02-23 10:28:25 +0100

    completo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log('Simon dice');
 
 function getRandomIntInclusive(min, max) {
-    min = Math.cell(min);
+    min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -10,6 +10,7 @@ const min=1;
 const max=100;
 const numeriDaIndovinare = [];
 const numeriUtente = [];
+const numeriIndovinati = [];
 
 do {
 
@@ -27,8 +28,18 @@ alert(`Memorizza questi 5 numeri: ${numeriDaIndovinare.join(',')}.`);
 setTimeout( ()=> {
 
     do {
+
+        const num = parseInt(prompt('Inserisci un numero.'));
+        numeriUtente.push(num);
+
+        if(numeriDaIndovinare.includes(num) && !numeriIndovinati.includes( num ) ) {
+            numeriIndovinati.push(num);
+        }
         
     } while (numeriUtente.length < numeriDaIndovinare.length);
-    const num = parseInt(prompt('Inserisci un numero.'));
+    console.log(numeriUtente, numeriIndovinati);
+    
+    alert(`Hai indovinato in totale ${numeriIndovinati.length}numeri.
+    I numeri indovinati sono: ${numeriIndovinati.join(',')}.` )
 }, 2000);
     
